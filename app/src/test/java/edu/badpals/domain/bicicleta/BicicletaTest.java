617fabe5a8e1a387957596104348b846cd2c9e00
a23sergiopg <serprigar@gmail.com>
2024-02-29 11:29:10 +0100

    test(BicicletaTest.java): add last test cases for Bicicleta.java and Movil interface

--- app/src/test/java/edu/badpals/domain/bicicleta/BicicletaTest.java
+++ app/src/test/java/edu/badpals/domain/bicicleta/BicicletaTest.java
@@ -15,4 +15,14 @@ public class BicicletaTest {
     public void constructorBiciTest(){
         assertEquals (999, bici.getId());
     }
+    @Test
+    public void toStringTest() {
+        assertEquals("999", bici.toString());
+    }
+
+    @Test
+    public void implementationMovilTest() {
+        Movil movil = bici;
+        assertEquals(999, movil.getId());
+    }
 }
