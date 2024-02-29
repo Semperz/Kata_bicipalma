423015048ff12e31cbaeb955ce0a8d793122f0cd
a23sergiopg <serprigar@gmail.com>
2024-02-29 12:23:24 +0100

    test(AnclajeTest.java): add test cases for Anclaje class

---
+++ app/src/test/java/edu/badpals/domain/estacion/AnclajeTest.java
@@ -0,0 +1,33 @@
+package edu.badpals.domain.estacion;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import edu.badpals.domain.bicicleta.Bicicleta;
+public class AnclajeTest {
+    @Test
+    public void anclarBiciTest() {
+        Anclaje anclaje = new Anclaje();
+        anclaje.anclarBici(new Bicicleta(911));
+        assertTrue(anclaje.isOcupado());
+    }
+    @Test
+    public void getBiciTest() {
+        Anclaje anclaje = new Anclaje();
+        anclaje.anclarBici(new Bicicleta(911));
+        assertTrue(anclaje.isOcupado());
+        anclaje.getBici();
+        assertTrue(anclaje.isOcupado());
+    }
+    @Test
+    public void liberarBiciTest() {
+        Anclaje anclaje = new Anclaje();
+        anclaje.anclarBici(new Bicicleta(911));
+        assertTrue(anclaje.isOcupado());
+
+        anclaje.liberarBici();
+        assertFalse(anclaje.isOcupado());
+    }
+}
