01f17502552dc7710794d7315683b44decfe61bb
a23sergiopg <serprigar@gmail.com>
2024-02-29 12:59:07 +0100

    feat(TarjetaUsuario.java): add TarjetaUsuario class

---
+++ app/src/main/java/edu/badpals/domain/tarjetausuario/TarjetaUsuario.java
@@ -0,0 +1,24 @@
+package edu.badpals.domain.tarjetausuario;
+
+public class TarjetaUsuario {
+    private final String id;
+    private boolean activada = false;
+
+    public TarjetaUsuario(String id, boolean activada) {
+        this.id = id;
+        this.activada = activada;
+    }
+
+    public boolean isActivada(){
+        return this.activada;
+    }
+
+    public void setActivada(boolean activada) {
+        this.activada = activada;
+    }
+
+    @Override
+    public String toString() {
+        return this.id;
+    }
+}
--- app/src/main/java/edu/badpals/tarjetausuario/TarjetaUsuario.java
+++
@@ -1,4 +0,0 @@
-package edu.badpals.tarjetausuario;
-
-public class TarjetaUsuario {
-}
