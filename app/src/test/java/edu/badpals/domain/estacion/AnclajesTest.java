159e4d9a105e34da21feb73b71fc9c69ef2a1810
a23sergiopg <serprigar@gmail.com>
2024-03-04 10:51:26 +0100

    test(AnclajesTest.java): add test cases for Anclajes

---
+++ app/src/test/java/edu/badpals/domain/estacion/AnclajesTest.java
@@ -0,0 +1,14 @@
+package edu.badpals.domain.estacion;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AnclajesTest {
+
+    @Test
+    public void toStringTest() {
+        Anclajes anclajes = new Anclajes(6);
+        assertEquals("Numero de anclajes: 6", anclajes.toString());
+    }
+}
